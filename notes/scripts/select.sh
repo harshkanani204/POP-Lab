037feb05fc6cb6e7283fdcdb9608e902276dbb50
Piyush P Kurur <ppk@cse.iitk.ac.in>
2020-08-10 13:12:06 +0530

    updated the select scripts a bit.

--- notes/scripts/select.sh
+++ notes/scripts/select.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-input=$1
+input="$1"
 output=$(echo $1 | sed s/.mp4/-cut.mp4/)
-start=$2
-end=$3
-ffmpeg  -i "$input" -ss "$start" -t "$end" -async 1  "$output"
+start="$2"
+end="$3"
+ffmpeg  -i "$input" -ss "$start" -to "$end" -async 1  "$output"
