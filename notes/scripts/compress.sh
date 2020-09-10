3f1dffd89898d8fd0711ea0cea06e84a1762221f
Piyush P Kurur <ppk@cse.iitk.ac.in>
2020-09-10 13:39:49 +0530

    compression script also does enhancing.

--- notes/scripts/compress.sh
+++ notes/scripts/compress.sh
@@ -5,9 +5,9 @@
 
 input=$1
 output=`echo $1 | sed s/.ogv/.mp4/`
-
+audiofilter="lowpass=3000,highpass=200"
 # ffmpeg -i "$input"  -max_muxing_queue_size 1024 -ab 128k -ac 2 -vcodec libx265 -preset slow -crf 22 -threads 0 "$output"
 
 
 # removed the -preset slow command.
-ffmpeg -i "$input"  -max_muxing_queue_size 1024 -ab 128k -ac 2 -vcodec libx265 -crf 22 -threads 0 "$output"
+ffmpeg -i "$input"  -max_muxing_queue_size 1024 -ab 128k -ac 2 -af "$audiofilter" -vcodec libx265 -crf 22 -threads 0 "$output"
